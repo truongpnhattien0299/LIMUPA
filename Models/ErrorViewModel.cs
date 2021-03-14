a53c285e34f280731db94ecf65cf90a040884a82
Tiến Trương <truongpnhattien0299@gmail.com>
2021-03-14 19:28:09 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TruongPhamNhatTien_3117410256.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+LIMUPA web commerce
