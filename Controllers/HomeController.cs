ebc5b98a2453159ca407c28c7a77a9f78f89d940
Tiến Trương <truongpnhattien0299@gmail.com>
2021-03-14 23:31:42 +0700

    login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using TruongPhamNhatTien_3117410256.Data;
 using TruongPhamNhatTien_3117410256.Models;
 
 namespace TruongPhamNhatTien_3117410256.Controllers
@@ -12,10 +13,11 @@ namespace TruongPhamNhatTien_3117410256.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private CommerceDbContext _context;
+        public HomeController(ILogger<HomeController> logger, CommerceDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -25,6 +27,21 @@ namespace TruongPhamNhatTien_3117410256.Controllers
 
         public IActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                
+                var result = _context.User.Where(s=>s.Username.Equals(user.Username) && s.Password.Equals(user.Password)).ToList();
+                if(result.Count()==1)
+                {
+                    var nameOfUser = from s in _context.User select s;
+                    return Redirect("/home");
+                }
+            }
             return View();
         }
 
--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace TruongPhamNhatTien_3117410256.Models
 {
     public class User
